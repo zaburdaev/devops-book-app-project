70ffd32c9fcf2cbdab2c22f9fd8f913c7d1d4cfa
zaburdaev <zaburdaev.vitaliy@gmail.com>
2026-03-03 21:48:51 +0200

    Add terraform folder and main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1" # Франкфурт
+}
+
+# Группа безопасности (открываем порты)
+resource "aws_security_group" "books_sg" {
+  name        = "books-app-sg-unique"
+  description = "Allow SSH and Web traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Создаем сервер EC2
+resource "aws_instance" "books_server" {
+  ami           = "ami-0084a47cc718c111a" # Ubuntu 22.04 LTS
+  instance_type = "t2.micro"             # Free Tier
+
+  vpc_security_group_ids = [aws_security_group.books_sg.id]
+
+  tags = {
+    Name = "BooksAppServer"
+  }
+}
+
+# Выводим IP сервера
+output "instance_public_ip" {
+  value = aws_instance.books_server.public_ip
+}
